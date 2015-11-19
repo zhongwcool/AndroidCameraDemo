290ac882129db47d51100592337c3d7fc12f0dcb
王烨明 <1398738509@qq.com>
2015-11-20 01:07:13 +0800

    引入camera2

---
+++ app/src/main/java/com/wangyeming/androidcamerademo/Camera/photo/CameraUtil.java
@@ -0,0 +1,49 @@
+package com.wangyeming.androidcamerademo.Camera.photo;
+
+import android.hardware.Camera;
+import android.util.Log;
+
+/**
+ * Created by Wang Yeming on 2015/11/19.
+ */
+public final class CameraUtil {
+
+    public static final int INVALID_CAMERA_ID = -1;
+
+    public static int findFrontFacingCamera() {
+        int cameraId = INVALID_CAMERA_ID;
+        // Search for the front facing camera
+        int numberOfCameras = Camera.getNumberOfCameras();
+        for (int i = 0; i < numberOfCameras; i++) {
+            Camera.CameraInfo info = new Camera.CameraInfo();
+            Camera.getCameraInfo(i, info);
+            if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+                Log.d("CameraUtil", "Camera found");
+                cameraId = i;
+                break;
+            }
+        }
+        return cameraId;
+    }
+
+    public static int findBackFacingCamera() {
+        int cameraId = INVALID_CAMERA_ID;
+        // Search for the front facing camera
+        int numberOfCameras = Camera.getNumberOfCameras();
+        for (int i = 0; i < numberOfCameras; i++) {
+            Camera.CameraInfo info = new Camera.CameraInfo();
+            Camera.getCameraInfo(i, info);
+            if (info.facing == Camera.CameraInfo.CAMERA_FACING_BACK) {
+                Log.d("CameraUtil", "Camera found");
+                cameraId = i;
+                break;
+            }
+        }
+        return cameraId;
+    }
+
+    public static boolean isCameraIdValid(int cameraId) {
+        return cameraId != INVALID_CAMERA_ID;
+    }
+
+}
--- app/src/main/java/com/wangyeming/androidcamerademo/MainActivity.java
+++ app/src/main/java/com/wangyeming/androidcamerademo/MainActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-import com.wangyeming.androidcamerademo.util.TakePhotoUtil;
+import com.wangyeming.androidcamerademo.Camera.util.TakePhotoUtil;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
