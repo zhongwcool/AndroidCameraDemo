e662a973738f1a08fadcc98dba0cd25a1d923184
Alex <zhongwcool@gmail.com>
2017-12-13 17:24:25 +0800

    Upgrade sdk level to update.

--- app/src/main/java/com/wangyeming/androidcamerademo/MainActivity.java
+++ app/src/main/java/com/wangyeming/androidcamerademo/MainActivity.java
@@ -1,8 +1,10 @@
 package com.wangyeming.androidcamerademo;
 
+import android.Manifest;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -13,12 +15,22 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.wangyeming.simplecamera.TakePhotoUtils;
 
+import java.util.List;
+
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.AppSettingsDialog;
+import pub.devrel.easypermissions.EasyPermissions;
+
 /**
  * 主页面
  */
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, EasyPermissions.PermissionCallbacks {
 
     private static final int REQUEST_CODE_FOR_CAMERA = 1;
+    private static final int RC_CAMERA = 123;
+    private String[] mPerms = {
+            Manifest.permission.CAMERA,
+    };
 
     private TextView vPath;
     private ImageView vPreview;
@@ -41,11 +53,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        int id = v.getId();
-        if (id == R.id.camera_route_camera) {
-            TakePhotoUtils.takePhotoByCamera(this, REQUEST_CODE_FOR_CAMERA);
-        } else if (id == R.id.camera_route_camera2) {
-            TakePhotoUtils.takePhotoByCamera2(this, REQUEST_CODE_FOR_CAMERA);
+        if (hasRequiredPermission()) {
+            int id = v.getId();
+            if (id == R.id.camera_route_camera) {
+                TakePhotoUtils.takePhotoByCamera(this, REQUEST_CODE_FOR_CAMERA);
+            } else if (id == R.id.camera_route_camera2) {
+                TakePhotoUtils.takePhotoByCamera2(this, REQUEST_CODE_FOR_CAMERA);
+            }
         }
     }
 
@@ -70,5 +84,45 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         }
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        // Forward results to EasyPermissions
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+
+    @Override
+    public void onPermissionsGranted(int requestCode, @NonNull List<String> perms) {
+
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, @NonNull List<String> perms) {
+        // (Optional) Check whether the user denied any permissions and checked "NEVER ASK AGAIN."
+        // This will display a dialog directing them to enable the permission in app settings.
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            new AppSettingsDialog.Builder(this).build().show();
+        }
+    }
+
+    @AfterPermissionGranted(RC_CAMERA)
+    private boolean hasRequiredPermission() {
+
+        if (EasyPermissions.hasPermissions(this, mPerms)) {
+            // Already have permission, do the thing
+            return true;
+        } else {
+            // Do not have permissions, request them now
+            EasyPermissions.requestPermissions(
+                    this,
+                    getString(R.string.camera_rationale),
+                    RC_CAMERA,
+                    mPerms
+            );
+            return false;
+        }
     }
 }
